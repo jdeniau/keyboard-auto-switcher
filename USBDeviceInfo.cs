6dfa5354e83b84e9e32e992c19994af243232b4b
Julien Deniau <julien.deniau@gmail.com>
2025-11-24 22:28:08 +0100

    remove dead code

--- USBDeviceInfo.cs
+++ USBDeviceInfo.cs
@@ -1,46 +1,9 @@
 using System.Management; // need to add System.Management to your project references.
 
-class USBDeviceInfo
+static class USBDeviceInfo
 {
-
+    // The instance ID prefix for the TypeMatrix target keyboard device
     public static string KeyboardInstanceName = "USB\\VID_1E54&PID_2030\\";
-
-    public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
-    {
-        this.DeviceID = deviceID;
-        this.PnpDeviceID = pnpDeviceID;
-        this.Description = description;
-    }
-    public string DeviceID { get; private set; }
-    public string PnpDeviceID { get; private set; }
-    public string Description { get; private set; }
-
-
-    public static List<USBDeviceInfo> GetUSBDevices()
-    {
-        List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
-
-        using var searcher = new ManagementObjectSearcher(
-            @"Select * From Win32_USBHub");
-        using ManagementObjectCollection collection = searcher.Get();
-
-        foreach (ManagementObject device in collection)
-        {
-            try
-            {
-                devices.Add(new USBDeviceInfo(
-                    (string)device.GetPropertyValue("DeviceID"),
-                    (string)device.GetPropertyValue("PNPDeviceID"),
-                    (string)device.GetPropertyValue("Description")
-                    ));
-            }
-            finally
-            {
-                device?.Dispose(); // Fix memory leak: dispose ManagementObject
-            }
-        }
-        return devices;
-    }
 
     /// <summary>
     /// Check if the target keyboard is currently connected
